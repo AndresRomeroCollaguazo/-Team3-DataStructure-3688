b2a2516af35a8a4a4058a5540742431729d50d0f
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-05-20 12:11:03 -0500

    Update Constructor.cpp

--- I-PARCIAL/LAB_1/Constructor.cpp
+++ I-PARCIAL/LAB_1/Constructor.cpp
@@ -1,1 +1,21 @@
-#include <stdio.h>
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operaciones Básicas
+Fecha creación: 19/05/2021
+Fecha modificación: 20/05/2021*/
+
+Operaciones(tipoDato _valor1,tipoDato _valor2){
+			valor1=_valor1;
+			valor2=_valor2;
+		}	
+void Operaciones::setOperaciones(tipoDato _valor1,tipoDato _valor2){
+			valor1=_valor1;
+			valor2=_valor2;
+		}
+ tipoDato valor1::getvalor1(){
+		return valor1;
+}
+tipoDato valor2::getvalor2(){
+		return valor2;
+}
