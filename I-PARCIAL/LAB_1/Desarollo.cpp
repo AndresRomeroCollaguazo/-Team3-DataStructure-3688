66a7996710090db04889070b8558bf4a6b0a60a9
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-05-19 23:26:20 -0500

    Update Desarollo.cpp

--- I-PARCIAL/LAB_1/Desarollo.cpp
+++ I-PARCIAL/LAB_1/Desarollo.cpp
@@ -1,1 +1,22 @@
 #include <stdio.h>
+// Realizar pruebas y mejorar 
+
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::suma(){
+	return valor1+valor2;
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::resta(){
+	return valor1-valor2;
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::multiplicacion(){
+	return valor1*valor2;
+}
+template <typename tipoDato>
+tipoDato Operaciones<tipoDato>::division(){
+	if(valor2==0){
+		cout<<"No se puede dividir !!!!"<<endl;
+	}else
+	return valor1/valor2;
+}
