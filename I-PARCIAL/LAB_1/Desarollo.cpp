282a866c7627230a6ed481ebfe5f8b631a6fb420
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-05-20 16:34:50 -0500

    Update Desarollo.cpp

--- I-PARCIAL/LAB_1/Desarollo.cpp
+++ I-PARCIAL/LAB_1/Desarollo.cpp
@@ -1,3 +1,10 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operaciones Básicas
+Fecha creación: 19/05/2021
+Fecha modificación: 20/05/2021*/
+
 #include <stdio.h>
 // Realizar pruebas y mejorar 
 
