ccfcf18301f72b24201ef0fcf36984788a7a397f
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-05-20 17:18:16 -0500

    Update Constructor.cpp

--- I-PARCIAL/LAB_1/Constructor.cpp
+++ I-PARCIAL/LAB_1/Constructor.cpp
@@ -5,17 +5,3 @@ Taller de Operaciones Básicas
 Fecha creación: 19/05/2021
 Fecha modificación: 20/05/2021*/
 
-Operaciones(tipoDato _valor1,tipoDato _valor2){
-			valor1=_valor1;
-			valor2=_valor2;
-		}	
-void Operaciones::setOperaciones(tipoDato _valor1,tipoDato _valor2){
-			valor1=_valor1;
-			valor2=_valor2;
-		}
- tipoDato valor1::getvalor1(){
-		return valor1;
-}
-tipoDato valor2::getvalor2(){
-		return valor2;
-}
