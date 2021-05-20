93e783dec1e06fb30e2f27aaf8672043d787a271
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-20 17:18:44 -0500

    Update Constructor.cpp

--- I-PARCIAL/LAB_1/Constructor.cpp
+++ I-PARCIAL/LAB_1/Constructor.cpp
@@ -5,3 +5,43 @@ Taller de Operaciones Básicas
 Fecha creación: 19/05/2021
 Fecha modificación: 20/05/2021*/
 
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+template <typename tipoDato>
+class Operaciones{
+	#include "estructura.h"
+	public :
+		Operaciones(tipoDato _valor1, tipoDato _valor2)
+		{
+			valor1=_valor1;
+			valor2=_valor2;
+		}
+		void setValor1(tipoDato valor_1)
+		{
+			valor1=valor_1;
+		}
+		tipoDato getValor1()
+		{
+			return valor1;
+		}
+		void setValor2(tipoDato valor_2)
+		{
+			valor2=valor_2;
+		}
+		tipoDato getValor2()
+		{
+			return valor2;
+		}
+		string to_String(){
+			//return std::to_string(3.1415926);
+		}
+		#include "operaciones.h"
+		
+	
+		
+};
