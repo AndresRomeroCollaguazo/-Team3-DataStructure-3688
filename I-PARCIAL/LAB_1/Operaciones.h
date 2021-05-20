e75e89d5c94c2ccd429715445117935d9442bb3b
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-20 11:31:05 -0500

    Update Operaciones.h

--- I-PARCIAL/LAB_1/Operaciones.h
+++ I-PARCIAL/LAB_1/Operaciones.h
@@ -1,4 +1,10 @@
-#include <stdio.h>
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operaciones Básicas
+Fecha creación: 19/05/2021
+Fecha modificación: 20/05/2021*/
+
 tipoDato suma();
 tipoDato resta();
 tipoDato multiplicacion();
