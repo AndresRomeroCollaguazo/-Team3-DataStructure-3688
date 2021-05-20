37be6e1b3046080ed48ca6c7443d403cfc5f289a
MichelleCantuna <84419131+MichelleCantuna@users.noreply.github.com>
2021-05-20 11:03:19 -0500

    Update Structura.h

--- I-PARCIAL/LAB_1/Structura.h
+++ I-PARCIAL/LAB_1/Structura.h
@@ -1,1 +1,19 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operaciones Básicas
+Fecha creación: 19/05/2021
+Fecha modificación: 20/05/2021*/
+
+#include <iostream>
 #include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+using namespace std;
+template <typename tipoDato>
+class Operaciones{
+	private:
+tipoDato valor1;
+tipoDato valor2;
+}
+
