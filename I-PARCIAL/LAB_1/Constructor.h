61e619f8c15d61fc3d7758809fe51f3d49947665
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-20 19:50:12 -0500

    Update and rename Constructor.cpp to Constructor.h

--- I-PARCIAL/LAB_1/Constructor.h
+++ I-PARCIAL/LAB_1/Constructor.h
@@ -4,7 +4,6 @@ Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Vala
 Taller de Operaciones Básicas
 Fecha creación: 19/05/2021
 Fecha modificación: 20/05/2021*/
-
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
@@ -14,30 +13,15 @@ using namespace std;
 
 template <typename tipoDato>
 class Operaciones{
-	#include "Structura.h"
+	private :
+		tipoDato valor1;
+		tipoDato valor2;
 	public :
-		Operaciones(tipoDato _valor1, tipoDato _valor2)
-		{
-			valor1=_valor1;
-			valor2=_valor2;
-			valor1=valor_1;
-		}
-		void setValor1(tipoDato valor_1)
-		{
-			valor1=valor_1;
-		}
-		tipoDato getValor1()
-		{
-			return valor1;
-		}
-		void setValor2(tipoDato valor_2)
-		{
-			valor2=valor_2;
-		}
-		tipoDato getValor2()
-		{
-			return valor2;
-		}
-		
+		Operaciones(tipoDato _valor1, tipoDato _valor2);
+		void setValor1(tipoDato _valor1);
+		tipoDato getValor1();
+		void setValor2(tipoDato _valor2);
+		tipoDato getValor2();
+		string to_String();
 		#include "Operaciones.h"
 };
