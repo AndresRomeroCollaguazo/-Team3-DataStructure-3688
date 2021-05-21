3081e3c6e94e59bba4329f15cdfef9b4655b3bc6
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-20 21:32:55 -0500

    Update Structura.cpp

--- I-PARCIAL/LAB_1/Structura.cpp
+++ I-PARCIAL/LAB_1/Structura.cpp
@@ -13,27 +13,27 @@ Fecha modificación: 20/05/2021*/
 template <typename tipoDato>
 Operaciones <tipoDato>::Operaciones(tipoDato _valor1, tipoDato _valor2){
 			valor1=_valor1;
-			valor2=_valor2;
+			valor2=_valor2;//@constructor
 
 }
 template <typename tipoDato>
 void Operaciones <tipoDato>::setValor1(tipoDato valor_1){
-	valor1=valor_1;
+	valor1=valor_1;//@assign generic data
 
 }
 template <typename tipoDato>
 tipoDato Operaciones  <tipoDato>::getValor1(){
-	return valor1;
+	return valor1;//@return generic data
 
 }
 template <typename tipoDato>
 void Operaciones  <tipoDato>::setValor2(tipoDato valor_2){
-	valor2=valor_2;
+	valor2=valor_2;//@assign generic data
 
 }
 template <typename tipoDato>
 tipoDato Operaciones  <tipoDato>::getValor2(){
-	return valor2;
+	return valor2;//@return generic data
 
 }
 
