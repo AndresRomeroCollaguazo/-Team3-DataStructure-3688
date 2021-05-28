6fb8492cabfd77fb0837d9417cf47f8f5ea53204
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-27 20:41:44 -0500

    Update and rename Programa3.cpp to Main.cpp

--- I-PARCIAL/LAB_2/Main.cpp
+++ I-PARCIAL/LAB_2/Main.cpp
@@ -7,8 +7,8 @@ Fecha creación: 26/05/2021
 Fecha modificación: 27/05/2021*/
 
 #include <iostream>
-#include "Operaciones.h"
-#include "Datos.h"
+#include "Operations.h"
+#include "Data.h"
 
 
 int main()
@@ -16,9 +16,9 @@ int main()
 	double d = 20.89;
 	float b = 15.5f;
 	int a = 5, b1 = 6;
-	Datos<float> dato(b, d);
-	Operaciones operacion(dato);
-	printf("\nLa suma es %.2f", operacion.operator +());
-	printf("\nLa resta es %.2f", operacion.operator -());
+	Data<float> datum(b, d);
+	Operations operation(datum)
+	printf("\nLa suma es %.2f", operation.operator +());
+	printf("\nLa resta es %.2f", operation.operator -());
 	return 0;
 }
