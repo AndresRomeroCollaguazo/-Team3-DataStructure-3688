0f6b1c40bca6aaffd9ad4c36530894bf90312830
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-05-27 20:47:30 -0500

    Update Operaciones.cpp

--- I-PARCIAL/LAB_2/Operaciones.cpp
+++ I-PARCIAL/LAB_2/Operaciones.cpp
@@ -1,5 +1,10 @@
+/ * Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operadores Sobrecargados 
+Fecha creación: 26/05/2021
+Fecha de modificación: 27/05/2021 * /
 #include "Operations.h"
-
 Operations::Operaciones(Date<float> newdatum){
 	this->data_set = newdate;
 }
