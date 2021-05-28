1a71b477c8504e6fd6b28c2422e425dc781ec5e0
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-05-27 23:36:57 -0500

    Update OperatorM.cpp

--- I-PARCIAL/LAB_3/OperatorM.cpp
+++ I-PARCIAL/LAB_3/OperatorM.cpp
@@ -1,3 +1,12 @@
+/***********************************************************************/
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Taller de Operadores Sobrecargados
+Fecha creación: 26/05/2021
+Fecha modificación: 27/05/2021*/
+/***********************************************************************/
+
 #include "OperatorM.h"
 #include<iostream>
 #include "OperatorM.h"
