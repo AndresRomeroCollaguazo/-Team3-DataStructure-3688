32b38e6cab8ce8484bf29985430ee21ccfddcc28
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-05-27 20:45:50 -0500

    Update Operaciones.cpp

--- I-PARCIAL/LAB_2/Operaciones.cpp
+++ I-PARCIAL/LAB_2/Operaciones.cpp
@@ -1,15 +1,12 @@
 #include "Operations.h"
 
-
-
-
 Operations::Operaciones(Date<float> newdatum){
 	this->data_set = newdate;
 }
 
 float Operations::operator +() {
 			return float(this->data_set.getDataA())+float(this->data_set.getDataB());
-			
+		
 }
 
 float Operations::operator -(){
