20c535d8fae020efc1244c6a89ed9c4dedd481f5
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-30 11:42:59 -0500

    Update Main.cpp

--- I-PARCIAL/DEBER_30-05/Main.cpp
+++ I-PARCIAL/DEBER_30-05/Main.cpp
@@ -14,15 +14,15 @@ int main()
 {
 	int vector[10] = {-1,5,8,10,20,6,7,8,9,10};
 	Vector v1(vector);
-	Operaciones op;
+	Operations op;
 	printf("Vector original\n");
-	op.imprimir(v1);
+	op.print(v1);
 	printf("\n");
-	op.ingresar(v1);
-	op.imprimir(v1);
+	op.get_into(v1);
+	op.print(v1);
 	printf("\n");
-	cout<<op.procesar(v1);
+	cout<<op.process(v1);
 	printf("\n\n");
-	op.encerar(v1);
-	op.imprimir(v1);
+	op.wax(v1);
+	op.print(v1);
 }
