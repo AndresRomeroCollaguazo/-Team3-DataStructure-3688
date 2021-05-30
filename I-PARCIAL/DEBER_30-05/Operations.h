8a9387a2d3438fbcfbe1ac4df67b962d454de0c9
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-30 11:24:33 -0500

    Add files via upload

---
+++ I-PARCIAL/DEBER_30-05/Operations.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Vector.h"
+class Operaciones
+{
+public:
+	Operaciones();
+	void encerar(Vector &);
+	void ingresar(Vector &);
+	float procesar(Vector&);
+	void imprimir(Vector &);
+};
+
