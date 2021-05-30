f7d6d0d1ce491f69b6a63674c87fb060dbc4cc48
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-30 13:38:50 -0500

    Update Main.cpp

--- I-PARCIAL/DEBER_30-05/Main.cpp
+++ I-PARCIAL/DEBER_30-05/Main.cpp
@@ -5,24 +5,24 @@ Deber de Memoria Estática
 Fecha creación : 28 / 05 / 2021
 Fecha de modificación : 30 / 05 / 2021 */
 
-	
+
 #include <iostream>
 #include "Vector.h";
 #include "Operations.h";
 using namespace std;
 int main()
 {
-	int vector[10] = {-1,5,8,10,20,6,7,8,9,10};
+	int vector[10] = { -1,5,8,10,20,6,7,8,9,10 };
 	Vector v1(vector);
 	Operations op;
-	printf("Vector original\n");
+	cout << "Vector original\n " << endl;
 	op.print(v1);
-	printf("\n");
+	cout << "\n" << endl;
 	op.get_into(v1);
 	op.print(v1);
-	printf("\n");
-	cout<<op.process(v1);
-	printf("\n\n");
+	cout << "\n" << endl;
+	cout << op.process(v1);
+	cout << "\n \n" << endl;
 	op.wax(v1);
 	op.print(v1);
 }
