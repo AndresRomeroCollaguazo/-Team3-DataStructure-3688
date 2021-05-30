6b7eb417dcffc953baa1cc6cd558ef9299eda9e6
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-30 11:48:13 -0500

    Update Main.cpp

--- I-PARCIAL/DEBER_30-05/Main.cpp
+++ I-PARCIAL/DEBER_30-05/Main.cpp
@@ -1,10 +1,10 @@
-/ * **************** ********
-/ * Universidad de las Fuerzas Armadas "ESPE"
+/* Universidad de las Fuerzas Armadas "ESPE"
 Software
-Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
 Deber de Memoria Estática
-Fecha creación: 28/05/2021
-Fecha de modificación: 30/05/2021 */
+Fecha creación : 28 / 05 / 2021
+Fecha de modificación : 30 / 05 / 2021 */
+
 	
 #include <iostream>
 #include "Vector.h";
