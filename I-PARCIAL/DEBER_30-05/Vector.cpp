643f09067f103651d4fb50cc51d28dbdc729bc0e
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-30 11:47:28 -0500

    Update Vector.cpp

--- I-PARCIAL/DEBER_30-05/Vector.cpp
+++ I-PARCIAL/DEBER_30-05/Vector.cpp
@@ -1,3 +1,10 @@
+/* Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Memoria Estática
+Fecha creación : 28 / 05 / 2021
+Fecha de modificación : 30 / 05 / 2021 */
+
 #include "Vector.h"
 
 Vector::Vector(int v[10])
