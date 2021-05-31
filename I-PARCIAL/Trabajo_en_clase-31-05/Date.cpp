f12ace6a1fd192e33976153777f8df0288cbaacb
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-31 10:56:36 -0500

    Update Date.cpp

--- I-PARCIAL/Trabajo_en_clase-31-05/Date.cpp
+++ I-PARCIAL/Trabajo_en_clase-31-05/Date.cpp
@@ -1,9 +1,10 @@
-/***********************************************************************
- * Module:  Date.cpp
- * Author:  avand
- * Modified: lunes, 31 de mayo de 2021 10:40:28
- * Purpose: Implementation of the class Date
- ***********************************************************************/
+
+/* Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Memoria Estática
+Fecha creación : 31 / 05 / 2021
+Fecha de modificación : 31 / 05 / 2021 */
 
 #include "Person.h"
 #include "Date.h"
