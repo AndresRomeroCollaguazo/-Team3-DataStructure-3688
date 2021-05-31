5a855c6ab57fbf599bfa45a967dcfe1463baa24b
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-31 11:16:00 -0500

    Update Main.cpp

--- I-PARCIAL/Trabajo_en_clase-31-05/Main.cpp
+++ I-PARCIAL/Trabajo_en_clase-31-05/Main.cpp
@@ -4,14 +4,15 @@ Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Val
 Taller de clases
 Fecha creación : 31 / 05 / 2021
 Fecha de modificación : 31 / 05 / 2021 */
-
+#include "Person.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-   Person p1;
-   Data  d1;
-   
+   Person p1("Jose", "Valdiviezo", 19, 1234);
+   Date d1(31, 05, 2021);
+   printf("La fecha es: %d / %d / %d", d1.getDay(), d1.getMonth(), d1.getYear());
+	printf("La persona es: %s %s edad: %d dni: %d", p1.getName(), p1.getLast_name(), p1.getAge(),p1.getDni());
    cout<<"FECHA: "<<endl;
 }
 
