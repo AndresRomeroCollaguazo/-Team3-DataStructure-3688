a4de6103bf823884edd176de81f25e204cb95200
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-05-31 18:46:44 -0500

    Add files via upload

---
+++ I-PARCIAL/Op_Matrices_31-05/Fuctions.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Module:  Fuctions.h
+ * Author:  avand
+ * Modified: lunes, 31 de mayo de 2021 18:14:39
+ * Purpose: Declaration of the class Fuctions
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_Fuctions_h)
+#define __Class_Diagram_1_Fuctions_h
+
+class Matrix;
+
+#include <Matrix.h>
+
+class Fuctions
+{
+public:
+   void multiplication(Matrix m1, Matrix m2);
+
+   Matrix* Matrix;
+
+protected:
+private:
+
+};
+
+#endif
---
+++ I-PARCIAL/Op_Matrices_31-05/Matrix.cpp
@@ -0,0 +1,69 @@
+/***********************************************************************
+ * Module:  Matrix.cpp
+ * Author:  avand
+ * Modified: lunes, 31 de mayo de 2021 18:14:39
+ * Purpose: Implementation of the class Matrix
+ ***********************************************************************/
+
+#include "Matrix.h"
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Matrix::getMatrix()
+// Purpose:    Implementation of Matrix::getMatrix()
+// Return:     int[]
+////////////////////////////////////////////////////////////////////////
+
+int[] Matrix::getMatrix(void)
+{
+   return matrix;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Matrix::setMatrix(int[] newMatrix)
+// Purpose:    Implementation of Matrix::setMatrix()
+// Parameters:
+// - newMatrix
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Matrix::setMatrix(int[] newMatrix)
+{
+   matrix = newMatrix;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Matrix::Matrix(int [][] _matrix)
+// Purpose:    Implementation of Matrix::Matrix()
+// Parameters:
+// - _matrix
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+Matrix::Matrix(int [][] _matrix)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Matrix::~Matrix()
+// Purpose:    Implementation of Matrix::~Matrix()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+Matrix::~Matrix()
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Matrix::enter_matrix(int[][] m1)
+// Purpose:    Implementation of Matrix::enter_matrix()
+// Parameters:
+// - m1
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Matrix::enter_matrix(int[][] m1)
+{
+   // TODO : implement
+}
