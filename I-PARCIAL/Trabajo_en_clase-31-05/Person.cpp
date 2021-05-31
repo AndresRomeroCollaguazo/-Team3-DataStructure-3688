1bf3dfa93bafba10970e0b6da5e507b2fac61ffd
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-31 10:57:17 -0500

    Update Person.cpp

--- I-PARCIAL/Trabajo_en_clase-31-05/Person.cpp
+++ I-PARCIAL/Trabajo_en_clase-31-05/Person.cpp
@@ -1,9 +1,9 @@
-/***********************************************************************
- * Module:  Person.cpp
- * Author:  avand
- * Modified: lunes, 31 de mayo de 2021 10:40:28
- * Purpose: Implementation of the class Person
- ***********************************************************************/
+/* Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Memoria Estática
+Fecha creación : 31 / 05 / 2021
+Fecha de modificación : 31 / 05 / 2021 */
 
 #include "Person.h"
 
