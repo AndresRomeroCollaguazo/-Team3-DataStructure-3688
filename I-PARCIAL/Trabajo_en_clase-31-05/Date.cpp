072a8fdd7575f3d9fea97e323ebe2781fab5d3a0
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-05-31 11:00:12 -0500

    Update Date.cpp

--- I-PARCIAL/Trabajo_en_clase-31-05/Date.cpp
+++ I-PARCIAL/Trabajo_en_clase-31-05/Date.cpp
@@ -2,7 +2,7 @@
 /* Universidad de las Fuerzas Armadas "ESPE"
 Software
 Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
-Deber de Memoria Estática
+Taller de clases
 Fecha creación : 31 / 05 / 2021
 Fecha de modificación : 31 / 05 / 2021 */
 
