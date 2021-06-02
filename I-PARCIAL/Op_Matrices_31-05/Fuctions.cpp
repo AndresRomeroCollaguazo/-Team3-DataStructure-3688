ceab7fcf68a9183760f25e19a2ec0a39d82beacc
MichelleCantuna <84419131+MichelleCantuna@users.noreply.github.com>
2021-06-01 21:24:33 -0500

    Update Fuctions.cpp

--- I-PARCIAL/Op_Matrices_31-05/Fuctions.cpp
+++ I-PARCIAL/Op_Matrices_31-05/Fuctions.cpp
@@ -5,11 +5,10 @@ Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Vala
 Deber de Multiplicacion de matrices
 Fecha creación: 31/05/2021
 Fecha de modificación: 01/06/2021 */
-
 /***********************************************************************
  * Module:  Fuctions.cpp
  * Author:  avand
- * Modified: lunes, 31 de mayo de 2021 18:14:39
+ * Modified: martes, 1 de junio de 2021 17:42:10
  * Purpose: Implementation of the class Fuctions
  ***********************************************************************/
 
@@ -17,85 +16,103 @@ Fecha de modificación: 01/06/2021 */
 #include "Fuctions.h"
 #include <iostream>
 using namespace std;
-class Matrix;
-////////////////////////////////////////////////////////////////////////
-// Name:       Fuctions::multiplication(Matrix m1, Matrix m2)
-// Purpose:    Implementation of Fuctions::multiplication()
-// Parameters:
-// - m1
-// - m2
-// Return:     void
-////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////
+ // Name:       Fuctions::multiplication(Matrix m1, Matrix m2, int row1, int column1, int column2)
+ // Purpose:    Implementation of Fuctions::multiplication()
+ // Parameters:
+ // - m1
+ // - m2
+ // - row1
+ // - column1
+ // - column2
+ // Return:     int**
+ ////////////////////////////////////////////////////////////////////////
 
-int** Fuctions::multiplication(Matrix& m1, Matrix& m2,int _fila1,int _columna1,int _columna2)
+int** Fuctions::multiplication(Matrix m1, Matrix m2, int row1, int column1, int column2)
 {
-
-    int** product = new int* [_fila1];
-    for (int i = 0; i < _fila1; i++) {
-        product[i] = new int[_columna2];
+    int** product = new int* [row1];
+    for (int i = 0; i < row1; i++) {
+        product[i] = new int[column2];
     }
 
-    for (int a = 0; a < _fila1; a++)
+    for (int a = 0; a < row1; a++)
     {
-        for (int b = 0; b < _columna2; b++)
+        for (int b = 0; b < column2; b++)
         {
-            int producto = 0;
-            for (int c = 0; c < _columna1; c++)
+            int sum = 0;
+            for (int c = 0; c < column1; c++)
             {
-                producto += m1.getMatrix(a, c) * m2.getMatrix(c, b);
+                sum += m1.rove_Matrix(a, c) * m2.rove_Matrix(c, b);
             }
 
-            product[a][b] = producto;
+            product[a][b] = sum;
         }
     }
     return product;
 }
 
-void Fuctions::imprimir(Matrix& m1,int _fila,int _columna) {
-    for (int i = 0; i < _fila; i++)
+////////////////////////////////////////////////////////////////////////
+// Name:       Fuctions::print(Matrix m1, int row, int column)
+// Purpose:    Implementation of Fuctions::print()
+// Parameters:
+// - m1
+// - row
+// - column
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Fuctions::print(Matrix m1, int row, int column)
+{
+    for (int i = 0; i < row; i++)
     {
         cout << " " << endl;
-        for (int j = 0; j < _columna; j++)
+        for (int j = 0; j < column; j++)
         {
             cout << "|";
-            cout << m1.getMatrix(i, j);
+            cout << m1.rove_Matrix(i, j);
 
         }cout << "|" << endl;
     }
 }
 
-void Fuctions::llenarMatriz(int _fila, int _columna,int ** _matriz)
+////////////////////////////////////////////////////////////////////////
+// Name:       Fuctions::fill_Matrix(int row, int column, int** m1)
+// Purpose:    Implementation of Fuctions::fill_Matrix()
+// Parameters:
+// - row
+// - column
+// - m1
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Fuctions::fill_Matrix(int row, int column, int** m1)
 {
     int b;
-    for (int l = 0; l < _fila; l++) {
-        for (int m = 0; m < _columna; m++) {
+    for (int l = 0; l < row; l++) {
+        for (int m = 0; m < column; m++) {
             cout << "Ingrese el valor [" << l << "][" << m << "] de la matriz" << endl;
             cin >> b;
-            _matriz [l][m] = b;
+            m1[l][m] = b;
         }
     }
 }
 
-int Fuctions::verificar(int _columna1,int _fila2) {
-    if (_columna1 == _fila2) {
+////////////////////////////////////////////////////////////////////////
+// Name:       Fuctions::verify(int column1, int row2)
+// Purpose:    Implementation of Fuctions::verify()
+// Parameters:
+// - column1
+// - row2
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int Fuctions::verify(int column1, int row2)
+{
+    if (column1 == row2) {
         return 1;
     }
     else {
         cout << "Numero de columas de la primera matriz no es igual al numero de filas de la segunda matriz" << endl;
         exit(-1);
     }
-
-}
-////////////////////////////////////////////////////////////////////////
-// Name:       Fuctions::multiplication(Matrix m1, Matrix m2)
-// Purpose:    Implementation of Fuctions::multiplication()
-// Parameters:
-// - m1
-// - m2
-// Return:     void
-////////////////////////////////////////////////////////////////////////
-
-void Fuctions::multiplication(Matrix& m1, Matrix& m2)
-{
-   // TODO : implement
 }
