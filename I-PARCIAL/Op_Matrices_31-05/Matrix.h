5ce8cdfcb4104a52d0f1be60a6d58faeefeb58f3
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-06-02 07:18:21 -0500

    Update Matrix.h

--- I-PARCIAL/Op_Matrices_31-05/Matrix.h
+++ I-PARCIAL/Op_Matrices_31-05/Matrix.h
@@ -1,4 +1,10 @@
-
+/***********************************************************************
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Multiplicacion de matrices
+Fecha creación: 31/05/2021
+Fecha de modificación: 01/06/2021 */
 /***********************************************************************
  * Module:  Matrix.h
  * Author:  avand
