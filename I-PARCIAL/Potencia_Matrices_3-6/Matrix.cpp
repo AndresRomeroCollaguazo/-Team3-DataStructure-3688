86758f0384684847dace8f9c926c20d403cd3b9c
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-06-03 23:31:57 -0500

    Update Matrix.cpp

--- I-PARCIAL/Potencia_Matrices_3-6/Matrix.cpp
+++ I-PARCIAL/Potencia_Matrices_3-6/Matrix.cpp
@@ -1,3 +1,10 @@
+  
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Potencia de matrices
+Fecha creación: 02/06/2021
+Fecha de modificación: 03/06/2021 */
 
 #include "Matrix.h"
 
