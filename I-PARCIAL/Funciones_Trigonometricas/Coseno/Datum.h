ca84666401e02fe15f018a85e1d3067db27e67f1
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-06-06 09:53:07 -0500

    Update Datum.h

--- I-PARCIAL/Funciones_Trigonometricas/Coseno/Datum.h
+++ I-PARCIAL/Funciones_Trigonometricas/Coseno/Datum.h
@@ -1,3 +1,10 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Funciones Trigonometricas
+Fecha creación: 04/06/2021
+Fecha de modificación: 06/06/2021 */
+
 /**
   * @file Datum.h
   * @version 1.0
