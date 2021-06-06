6fd1c1eb037d9c45316a8d23fff8fc42c98e0cc9
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-06-06 09:54:20 -0500

    Update CosenoRecursividad.cpp

--- I-PARCIAL/Funciones_Trigonometricas/Coseno/CosenoRecursividad.cpp
+++ I-PARCIAL/Funciones_Trigonometricas/Coseno/CosenoRecursividad.cpp
@@ -1,4 +1,9 @@
-
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Funciones Trigonometricas
+Fecha creación: 04/06/2021
+Fecha de modificación: 06/06/2021 */
 
 #include <iostream>
 #include "Datum.h"
