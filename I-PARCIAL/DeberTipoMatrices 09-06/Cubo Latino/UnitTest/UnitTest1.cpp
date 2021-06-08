db91b9f4c39101589d9b54aca439d138505ea201
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-06-08 16:19:21 -0500

    Add files via upload

---
+++ I-PARCIAL/DeberTipoMatrices 09-06/Cubo Latino/UnitTest/UnitTest1.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include "../Datum.h"
+#include "../Fuction.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int* vector = new int[10], i = 0, n, a1, a2, a3, a4;
+			cout << "CUADRADO LATINO\n";
+			cout << "Introduzca cantidad elementos: ";
+			cin >> n;
+			cout << "Ingrese los elementos (distintos): \n";
+			for (i = 0; i < n; i++) {
+				cin >> *(vector + i);
+			}
+			Datum d(vector);
+			Fuction f;
+
+			a1 = 0;
+			a2 = n - 1;
+			a3 = n;
+			a4 = n - 1;
+			i = f.cube(d.getVector(), a1, a2, a3, a4);
+			
+		}
+	};
+}
