69642b47573b6cfefbb4043f3a616e832a96d568
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-06-08 21:04:49 -0500

    Update Pascal.h

--- I-PARCIAL/DeberTipoMatrices 09-06/Triangulo de Pascal/Pascal.h
+++ I-PARCIAL/DeberTipoMatrices 09-06/Triangulo de Pascal/Pascal.h
@@ -1,6 +1,6 @@
 /***********************************************************************
  * Module:  Pascal.h
- * Author:  Jorge
+ * Author:  Martin
  * Modified: martes, 08 de junio de 2021 20:47:12
  * Purpose: Declaration of the class Pascal
  ***********************************************************************/
@@ -11,15 +11,15 @@
 class Pascal
 {
 public:
-   void Pacal(int _n, int _res);
-   void graficPascal(int** _matrix);
-   void verify(void);
-   int fact(int n);
-   int combination(int n, int r);
-   void pascal(int curline, int nlines, int** matrix, int row);
-   int get_n(void);
-   void set_n(int _n);
-   void pool(void);
+	Pascal(int,int);
+	void graficPascal(int**);
+	void verify();
+	int fact(int);
+	int combination(int , int);
+	void pascal(int , int ,int**,int);
+	int get_n();
+	void set_n(int );
+	void pool(int**);
 
 protected:
 private:
