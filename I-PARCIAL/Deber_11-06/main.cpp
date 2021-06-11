b51947df37a20885f356da1d3ee58f54624dff29
Hamilton Perez <84423196+Hamilton9910@users.noreply.github.com>
2021-06-10 23:22:43 -0500

    Add files via upload

---
+++ I-PARCIAL/Deber_11-06/main.cpp
@@ -0,0 +1,26 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Listas
+Fecha creación: 09/06/2021
+Fecha de modificación: 10/06/2021 */
+
+#include <iostream>
+#include "List.h"
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include "Control.h"
+using namespace std;
+
+int main()
+{  
+    Control c;
+    c.viewMenu();
+
+}
+
+
+
