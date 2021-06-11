a35d64aad2cef6771743fb58110c3f9c3280f567
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-06-10 23:12:04 -0500

    Update Nodo.h

--- I-PARCIAL/Deber_11-06/Nodo.h
+++ I-PARCIAL/Deber_11-06/Nodo.h
@@ -1,26 +1,67 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Listas
+Fecha creación: 09/06/2021
+Fecha de modificación: 10/06/2021 */
+/**
+  * @file Nodo.h
+  * @version 1.0
+  * @date 10-06-2021
+  * @author Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+  * @brief listas
+  * @title Lista
+  *
+  */
+#pragma once
 #pragma once
 #include <cstddef>
 
-/***********************************************************************
+/*************************
  * Module:  Nodo.h
  * Author:
- * Modified: miércoles, 09 de junio de 2021 19:54:24
+ * Modified: miÃ©rcoles, 09 de junio de 2021 19:54:24
  * Purpose: Declaration of the class Nodo
- ***********************************************************************/
+ *************************/
 
 #if !defined(__UML_Class_Diagram_2_Nodo_h)
 #define __UML_Class_Diagram_2_Nodo_h
-
+ /**
+	   * @brief Clase Nodo que contiene un nodo de los mismos y dato de tipo int
+	   */
 class Nodo
 {
 public:
+	/**
+	  * @brief Destructor de la clase Nodo
+	  */
 	~Nodo();
+	/**
+	  * @brief getDate Devuelve el contenido del atributo next
+	  * @return Devuelve el atributo next
+	  */
 	int getDate(void);
+	/**
+	  * @brief setDate Recibe el contenido del atributo newDate
+	  * @param newDate coloca un nuevo dato en el atributo
+	  */
 	void setDate(int newDate);
+	/**
+	  * @brief getNext Devuelve el contenido del atributo next
+	  * @return Devuelve el atributo next
+	  */
 	Nodo* getNext(void);
+	/**
+	  * @brief setNext Recibe el contenido del atributo newNext
+	  * @param newNext coloca un nuevo dato en el atributo
+	  */
 	void setNext(Nodo* newNext);
+	/**
+	  * @brief Constructor de la clase Nodo
+	  * @param value atributo nuevo del objeto
+	  */
 	Nodo(int value);
-	
+
 
 protected:
 private:
