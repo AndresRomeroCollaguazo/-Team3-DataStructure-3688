0cf502a485e1e375fe1e1dc5741139e4dd6169c4
AndresValarezo <84350542+AndresValarezo@users.noreply.github.com>
2021-06-10 23:16:27 -0500

    Update Control.h

--- I-PARCIAL/Deber_11-06/Control.h
+++ I-PARCIAL/Deber_11-06/Control.h
@@ -1,19 +1,63 @@
-/***********************************************************************
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Listas
+Fecha creación: 09/06/2021
+Fecha de modificación: 10/06/2021 */
+/**
+  * @file Control.h
+  * @version 1.0
+  * @date 10-06-2021
+  * @author Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+  * @brief listas
+  * @title Lista
+  *
+  */
+#pragma once
+/*************************
  * Module:  Control.h
- * Author:  
- * Modified: mi�rcoles, 09 de junio de 2021 18:38:18
+ * Author:
+ * Modified: miércoles, 09 de junio de 2021 18:38:18
  * Purpose: Declaration of the class Control
- ***********************************************************************/
-
+ *************************/
+#include <iostream>
+#include <windows.h>
+#define ARRIBA 'w'
+#define ABAJO 's'
+#define ENTER 13
 #if !defined(__UML_Class_Diagram_2_Control_h)
 #define __UML_Class_Diagram_2_Control_h
-
+ /**
+	   * @brief Clase Control que contiene el Menu del aplicativo
+	   */
 class Control
 {
 public:
-   void viewMenu(void);
-   void onlyIntControl(int _value);
-   void onlyCharControl(char _value);
+	/**
+	  * @brief viewMenu Muestrael menu
+	  * @param void
+	  */
+	void viewMenu(void);//menu
+	/**
+	  * @brief gotoxy Mueve el cursor tipo flecha
+	  * @param x, y Coordenadas de ubicacion de la flecha
+	  */
+	int gotoxy(USHORT x, USHORT y);
+	/**
+	  * @brief useMenu Despliega las opciones que contiene el Menu
+	  * @param titulo, opciones Son los nombres de las opciones que contiene el Menu
+	  */
+	int useMenu(const char titulo[], const char* opciones[], int n);
+	/**
+	  * @brief onlyIntControl Valida los numeros enteros ingresados desde teclado
+	  * @param char Solo admite numeros
+	  */
+	char* onlyIntControl(const char*);//validacion enteros
+	/**
+	  * @brief onlyCharControl Permite que el cursos se mueva de arriba hacia abajo
+	  * @param void
+	  */
+	char onlyCharControl(void);//sube baja 
 
 protected:
 private:
