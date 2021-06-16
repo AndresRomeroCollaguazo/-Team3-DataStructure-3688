e770a7a25ccb9f81a4f7a55729c6b9cbb39a8d5a
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-06-15 22:59:27 -0500

    Update Nodo.h

--- I-PARCIAL/Deber 15-06 Listas Doblemente Enlazadas/Nodo.h
+++ I-PARCIAL/Deber 15-06 Listas Doblemente Enlazadas/Nodo.h
@@ -1,32 +1,57 @@
-/***********************************************************************
- * Module:  Nodo.h
- * Author:  Jorge
- * Modified: martes, 15 de junio de 2021 16:19:04
- * Purpose: Declaration of the class Nodo
- ***********************************************************************/
-
-#if !defined(__UML_Class_Diagram_2_Nodo_h)
-#define __UML_Class_Diagram_2_Nodo_h
-
-class Nodo
-{
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Listas
+Fecha creación : 13 / 06 / 2021
+Fecha de modificación : 15 / 06 / 2021 */
+/**
+  *@file Nodo.h
+  * @version 1.0
+  * @fecha 15 - 06 - 2021
+  * @autor Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+  * @brief listasDobles
+  * @title Lista Dobles
+  *
+  */
+#pragma once
+/**
+  * @brief Clase Nodo que contiene un nodo de los mismos y dato de tipo int
+  */
+class Nodo {
 public:
-   ~Nodo();
-   int getDate(void);
-   void setDate(int newDate);
-   Nodo* getNext(void);
-   void setNext(Nodo* newNext);
-   Nodo(int value);
-   Nodo* getPrevious(void);
-   void setPrevious(Nodo* newPrevious);
-
-protected:
+	Nodo(int);
+	Nodo* getNext();
+	/**
+	  * @brief getNext Recibe el contenido del atributo newNext
+	  * @param newNext coloca un nuevo dato en el atributo
+	  */
+	void setNext(Nodo*);
+	/**
+	  * @brief setNext Devuelve el contenido del atributo next
+	  * @return Devuelve el atributo next
+	  */
+	Nodo* getPrevious();
+	/**
+	  * @brief getPrevious Recibe el contenido del atributo newPrevious
+	  * @param newPrevious coloca un nuevo dato en el atributo
+	  */
+	void setPrevious(Nodo*);
+	/**
+	  * @brief setPrevious Devuelve el contenido del atributo previous
+	  * @return Devuelve el atributo previous
+	  */
+	int getValue();
+	/**
+	  * @brief getValue Recibe el contenido del atributo newValue
+	  * @param newValue coloca un nuevo dato en el atributo
+	  */
+	void setValue(int);
+	/**
+	  * @brief setValue Devuelve el contenido del atributo value 
+	  * @return Devuelve el atributo value 
+	  */
 private:
-   int date;
-   Nodo* next;
-   Nodo* previous;
-
-
+	int value;
+	Nodo* next = nullptr;
+	Nodo* previous =nullptr;
 };
-
-#endif
