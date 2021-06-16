126832aef0da3a230e9b0fea279dd33181bcf8a6
Martin Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2021-06-15 23:41:53 -0500

    Update Double_linked_list.h

--- I-PARCIAL/Deber 15-06 Listas Doblemente Enlazadas/Double_linked_list.h
+++ I-PARCIAL/Deber 15-06 Listas Doblemente Enlazadas/Double_linked_list.h
@@ -1,41 +1,72 @@
-/***********************************************************************
- * Module:  Double_Linked_List.h
- * Author:  Jorge
- * Modified: martes, 15 de junio de 2021 16:38:21
- * Purpose: Declaration of the class Double_Linked_List
- ***********************************************************************/
-
-#if !defined(__UML_Class_Diagram_2_Double_Linked_List_h)
-#define __UML_Class_Diagram_2_Double_Linked_List_h
-
-class Nodo;
-
-#include <Nodo.h>
-
-class Double_Linked_List
-{
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+Deber de Listas
+Fecha creación : 13 / 06 / 2021
+Fecha de modificación : 15 / 06 / 2021 */
+/**
+/ **
+  *@file Lista_doble_enlazada.h
+  * @version 1.0
+  * @fecha 15 - 06 - 2021
+  * @autor Cantuña Michelle, Medina Martín, Pérez Hamilton, Romero Jorge y Valarezo Andrés
+  * @brief listasDobles
+  * @title Lista Dobles
+  */
+#pragma once
+#include "Nodo.h"
+#include <functional>
+/**
+  * @brief Clase ListaDobleEnlazada que contiene el primer y actual nodo
+  */
+class Double_linked_list {
 public:
-   void insert_to_Head(int value);
-   void rove(void);
-   bool emply_List(void);
-   void insert_Between(int value);
-   void insert_to_Tail(int value);
-   void consult(int value);
-   Double_Linked_List();
-   ~Double_Linked_List();
-   void delete(void);
-   void print(void* std::function<void_int,int_>callback);
-
-   Nodo* nodo;
-
-protected:
+	Double_linked_list() = default;
+	/**
+	  * @brief insertToHead Retorna los datos ingresados por la cabeza
+	  * @param _value Numeros a insertar
+	  * @return Devuelve los datos ingresados en la lista a traves de la cabeza
+	  */
+	void insertToHead(int);
+	/**
+	  * @brief insertToTail Retorna los datos ingresados por la cola
+	  * @param _value Numeros a insertar
+	  * @return Devuelve los datos ingresados en la lista a traves de la cola
+	  */
+	void insertToTail(int);
+	/**
+	  * @brief  insertMiddle Inserta elementos entre posiciones
+	  * @param _value posicion
+	  * @parum _value2 numero
+	  * @return 
+	  */
+	void insertMiddle(int,int);
+	/**
+	  * @brief consultar consulta los datos
+	  * @param
+	  * @return Devuelve los datos consultados
+	  */
+	void consult(int);
+	/**
+	  * @brief deleteL Elimina el dato ingresado 
+	  * @param
+	  * @return Devuelve los datos eliminados en la lista a traves de la cabeza
+	  */
+	void deleteL();
+	/**
+	  * @brief emptyList vacia la carga de los datos ingresados o eliminados
+	  * @param
+	  * @return Devuelve los lista vacia
+	  */
+	bool emptyList();
+	/**
+	  * @brief printList Muestra los datos ingresados o eliminados
+	  * @param
+	  * @return Devuelve los datos ingresados o eliminados de la lista
+	  */
+	void imprimir(std::function<void(int,int)>callback);
 private:
-   Nodo* last_Nodo(void);
-
-   int size;
-   Nodo* head;
-
-
+	Nodo* ultimo_Nodo();
+	Nodo* head = nullptr;
+	int size = 0;
 };
-
-#endif
