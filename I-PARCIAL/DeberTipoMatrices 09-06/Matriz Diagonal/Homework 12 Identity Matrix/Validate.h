f4398f448e245262d59a9802bbb80e9536a58094
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-06-18 08:48:33 -0500

    Add files via upload

---
+++ I-PARCIAL/DeberTipoMatrices 09-06/Matriz Diagonal/Homework 12 Identity Matrix/Validate.h
@@ -0,0 +1,20 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores: Grupo3
+Deber de Cubo latino
+Fecha creación : 04 / 06 / 2021
+Fecha de modificación : 18 / 06 / 2021 */
+#pragma once
+/**
+   * @brief Clase Validate contiene la funcion de ingresar
+   */
+class Validate {
+public:
+	/**
+		 * @brief ingresar Valida el ingreso solo de numeros
+		  * @param msj
+		  * @return Devuelve los datos ingresados, es decir solo numeros
+		  */
+	char* ingresar(const char*);
+
+};
