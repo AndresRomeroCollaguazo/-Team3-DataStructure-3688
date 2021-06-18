55f5a06b625ec7ed6921cebe2245be006836e4f4
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-06-18 08:40:44 -0500

    Add files via upload

---
+++ I-PARCIAL/DeberTipoMatrices 09-06/Triangulo de Pascal/Homework 11 Pascal Triangle/Operations.h
@@ -0,0 +1,37 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Ing. de Software
+Autor: Pérez Hámilton
+Deber de Matriz Diagonal
+Fecha creación : 06 / 06 / 2021
+Fecha de modificación : 08 / 06 / 2021 */
+
+/**
+  * @file Operations.h
+  * @version 1.0
+  * @date 08-06-2021
+  * @author Hamilton Dario Perez Zambrano
+  * @title Objeto operations
+  * @brief Clase Operations
+  *
+  */
+
+#pragma once
+#include <iostream>
+#include "Matrix.h"
+
+class Operations {
+private:
+
+public:
+
+	void matrixDrandom(int n);
+	/**
+	  * @brief matrixDrandom genera una matriz con doble diagonal con numeros aleatorios
+	  * @param n valor de ingreso
+	  */
+	void matrix1Di(int n);
+	/**
+	  * @brief matrix1Di genera una matriz con doble diagonal usando identidad
+	  * @param n valor de ingreso
+	  */
+};
