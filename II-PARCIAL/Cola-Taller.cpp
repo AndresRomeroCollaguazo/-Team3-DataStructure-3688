4a323d16b4feb8742cf7f9710515ec979378e826
AndresRomeroCollaguazo <84428775+AndresRomeroCollaguazo@users.noreply.github.com>
2021-07-12 09:14:58 -0500

    Add files via upload

---
+++ II-PARCIAL/Cola-Taller.cpp
@@ -0,0 +1,24 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores : CantuÃ±a Michelle, Medina MartÃ­n, PÃ©rez Hamilton, Romero Jorge y Valarezo AndrÃ©s
+Taller
+Fecha creaciÃ³n : 13 / 06 / 2021
+Fecha de modificaciÃ³n : 21 / 06 / 2021 */
+#include <iostream>
+#include "Control.h"
+#include "Tail.h"
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+
+    Control c;
+    c.activar();
+
+}
+
